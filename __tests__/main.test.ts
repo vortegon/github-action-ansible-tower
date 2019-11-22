ee055055e63425220bd71ff0a210ddc58d73b77c
Victor Ortegon <victor347@gmail.com>
2019-11-22 11:31:33 -0500

    remove tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,8 +1,8 @@
-import {wait} from '../src/wait'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 
+/* 
 test('throws invalid number', async() => {
     const input = parseInt('foo', 10);
     await expect(wait(input)).rejects.toThrow('milleseconds not a number');
@@ -24,4 +24,4 @@ test('test runs', () => {
         env: process.env
     };
     console.log(cp.execSync(`node ${ip}`, options).toString());
-});
+}); */
