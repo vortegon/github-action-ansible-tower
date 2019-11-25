b2706f6a6acd696601aed6e9f89c3c3554be3b36
Victor Ortegon <victor347@gmail.com>
2019-11-25 00:27:00 -0500

    change to generic ansible template deployment

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import * as core from '@actions/core';
-import fs = require('fs');
-import request = require('request-promise');
+import * as core from "@actions/core";
+import fs = require("fs");
+import request = require("request-promise");
 
 run();
 
@@ -11,8 +11,7 @@ async function run() {
     const jobData = await getFinalStatus(requestData, jobUrl);
     const output = await printAnsibleOutput(requestData, jobData);
     await exportResourceName(output);
-  }
-  catch (err) {
+  } catch (err) {
     if (err instanceof SyntaxError) {
       console.log(err.message);
       core.setFailed("Extra vars invalid format, please provide a valid JSON.");
@@ -23,16 +22,12 @@ async function run() {
 }
 
 async function getRequestData() {
-
-  const username: string = core.getInput('ansible-user');
-  const password: string = core.getInput('ansible-pass');
-  const url: string = 'https://tower.000ukso.sbp.eyclienthub.com/';
-  const subscription: string = core.getInput('azure-subscription');
-  const clientID: string = core.getInput('azure-client-id');
-  const clientSecret: string = core.getInput('azure-client-secret');
-  const additionalVars = JSON.parse(core.getInput('additional-vars'));
-  const templateId: string = core.getInput('template-id');
-  const certPath: string = core.getInput('certificate-path');
+  const username: string = core.getInput("ansible-tower-user");
+  const password: string = core.getInput("ansible-tower-pass");
+  const url: string = core.getInput("ansible-tower-url");
+  const additionalVars = JSON.parse(core.getInput("additional-vars"));
+  const templateId: string = core.getInput("template-id");
+  const certPath: string = core.getInput("certificate-path");
   let certBase64: any = null;
   const requestData: any = { extraVars: {}, templateId };
 
@@ -52,20 +47,19 @@ async function getRequestData() {
   });
 
   requestData.extraVars.extra_vars = {
-    ...(subscription != null && { var_azure_rm_subid: subscription }),
-    ...(clientID != null && { AZURE_RM_CLIENTID: clientID }),
-    ...(clientSecret != null && { AZURE_RM_SECRET: clientSecret }),
-    ...(certBase64 != null && { var_applicationGatewayFrontEndSslCertData: certBase64 })
+    ...(certBase64 != null && {
+      var_applicationGatewayFrontEndSslCertData: certBase64
+    })
   };
 
   Object.assign(requestData.extraVars.extra_vars, additionalVars);
 
   const extraVarsToPrint = Object.assign({}, requestData.extraVars.extra_vars);
   if (certPath) {
-    extraVarsToPrint.var_applicationGatewayFrontEndSslCertData = "*************";
+    extraVarsToPrint.var_applicationGatewayFrontEndSslCertData =
+      "*************";
   }
 
-  console.log("Ansible Tower:", url);
   console.log("extra-vars: ");
   console.log(extraVarsToPrint);
 
@@ -73,8 +67,9 @@ async function getRequestData() {
 }
 
 async function launchJob(requestData: any) {
-
-  console.log(`Launching Template ID ${requestData.templateId} on Ansible Tower...`);
+  console.log(
+    `Launching Template ID ${requestData.templateId} on Ansible Tower...`
+  );
 
   const options = {
     method: "POST",
@@ -86,21 +81,24 @@ async function launchJob(requestData: any) {
 
   if (response && response.job) {
     console.log(`Template Id ${requestData.templateId} launched successfully.`);
-    console.log(`Job ${response.job} was created on Ansible Tower: Status ${response.status}.`);
+    console.log(
+      `Job ${response.job} was created on Ansible Tower: Status ${response.status}.`
+    );
     return response.url;
-  }
-  else if (response && response.detail) {
-    console.log(`Template ID ${requestData.templateId} couldn't be launched, the Ansible API is returning the following error:`);
+  } else if (response && response.detail) {
+    console.log(
+      `Template ID ${requestData.templateId} couldn't be launched, the Ansible API is returning the following error:`
+    );
     throw new Error(response.detail);
-  }
-  else {
+  } else {
     console.log(response);
-    throw new Error(`Template ID ${requestData.templateId} couldn't be launched, the Ansible API is not working`);
+    throw new Error(
+      `Template ID ${requestData.templateId} couldn't be launched, the Ansible API is not working`
+    );
   }
 }
 
 async function getFinalStatus(requestData: any, jobUrl: string) {
-
   const options = {
     url: jobUrl,
     method: "GET"
@@ -109,7 +107,11 @@ async function getFinalStatus(requestData: any, jobUrl: string) {
   let response = await requestData.baseRequest(options);
 
   if (response && response.status) {
-    if (!(response.status == "failed") && !(response.status == "successful") && !(response.status == "error")) {
+    if (
+      !(response.status == "failed") &&
+      !(response.status == "successful") &&
+      !(response.status == "error")
+    ) {
       console.log("Validating Job status...");
       await wait(10000);
       console.log(`Job status: ${response.status}.`);
@@ -118,73 +120,79 @@ async function getFinalStatus(requestData: any, jobUrl: string) {
     } else {
       return response;
     }
-  }
-  else if (response && response.detail) {
+  } else if (response && response.detail) {
     console.log("Failed to get job status from Ansible Tower.");
     throw new Error(response.detail);
-  }
-  else {
+  } else {
     console.log(response);
     throw new Error("Failed to get job status from Ansible Tower.");
   }
 }
 
 async function printAnsibleOutput(requestData: any, jobData: any) {
-
   const options = {
-      url: `${jobData.related.stdout}?format=txt`,
-      method: "GET",
-      json: false,
+    url: `${jobData.related.stdout}?format=txt`,
+    method: "GET",
+    json: false
   };
 
   const response = await requestData.baseRequest(options);
 
   if (jobData.status == "failed" && response) {
-      console.log(`Final status: ${jobData.status}`);
-      console.log("***************************Ansible Tower error output***************************");
-      console.log(response);
-      throw new Error(`Ansible tower job ${jobData.id} execution failed`);
-  }
-  else if (jobData.status == "error") {
-      console.log(`Final status: ${jobData.status}`);
-      console.log("***************************Ansible Tower error output***************************");
-      console.log(response);
-      console.log("***************************Ansible Tower traceback output***************************");
-      console.log(jobData.result_traceback);
-      throw new Error(`An error has ocurred on Ansible tower trying to launch job ${jobData.id}`);
-  }
-  else if (jobData.status == "successful" && response) {
-      console.log(`Final status: ${jobData.status}`);
-      console.log("******************************Ansible Tower output******************************");
-      console.log(response);
-  }
-  else{
-      console.log(`Final status: ${jobData.status}`);
-      console.log("[warning]: An error ocurred trying to get the ansible tower output");
-      console.log(response);
+    console.log(`Final status: ${jobData.status}`);
+    console.log(
+      "***************************Ansible Tower error output***************************"
+    );
+    console.log(response);
+    throw new Error(`Ansible tower job ${jobData.id} execution failed`);
+  } else if (jobData.status == "error") {
+    console.log(`Final status: ${jobData.status}`);
+    console.log(
+      "***************************Ansible Tower error output***************************"
+    );
+    console.log(response);
+    console.log(
+      "***************************Ansible Tower traceback output***************************"
+    );
+    console.log(jobData.result_traceback);
+    throw new Error(
+      `An error has ocurred on Ansible tower trying to launch job ${jobData.id}`
+    );
+  } else if (jobData.status == "successful" && response) {
+    console.log(`Final status: ${jobData.status}`);
+    console.log(
+      "******************************Ansible Tower output******************************"
+    );
+    console.log(response);
+  } else {
+    console.log(`Final status: ${jobData.status}`);
+    console.log(
+      "[warning]: An error ocurred trying to get the ansible tower output"
+    );
+    console.log(response);
   }
   return response;
 }
 
 async function exportResourceName(output: string) {
-
   const regex = /(\/(\w+)\\)|(\/(\w+)")/g;
   const found = output.match(regex);
 
   if (found) {
-      const resourceName = found[found.length - 1].substring(1, found[found.length - 1].length - 1);
-      core.setOutput('RESOURCE_NAME', resourceName);
-      console.log("Resource name exported: "+ resourceName);
-  }
-  else {
-      console.log("[warning]: No resource name exported as output variable.");
+    const resourceName = found[found.length - 1].substring(
+      1,
+      found[found.length - 1].length - 1
+    );
+    core.setOutput("RESOURCE_NAME", resourceName);
+    console.log("Resource name exported: " + resourceName);
+  } else {
+    console.log("[warning]: No resource name exported as output variable.");
   }
 }
 
 async function base64_encode(file: string) {
-
   const bitmap = fs.readFileSync(file);
-  return new Buffer(bitmap).toString('base64');
+  return new Buffer(bitmap).toString("base64");
 }
 
 async function wait(ms: any) {
